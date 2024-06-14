9e488c1e55cebfc46ea0d8d47e8f431086cbd382
Andrew Huth <git@huth.me>
2024-06-14 09:50:44 -0400

    Show some toggleable checkboxes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,34 @@
+import {useReducer} from 'react';
+
 export default function App() {
+  const [values, dispatch] = useReducer(
+    function (prevState: boolean[], action: {type: 'toggle'; index: number}) {
+      if (action.type === 'toggle') {
+        const nextState = prevState.slice();
+        nextState[action.index] = !nextState[action.index];
+        return nextState;
+      }
+
+      return prevState;
+    },
+    [false, false, false, false, false, false, false, false, false, false],
+  );
+
   return (
     <main>
-      <h1>Hello world</h1>
+      <ol>
+        {values.map((value, i) => {
+          return (
+            <li key={i}>
+              <input
+                type="checkbox"
+                checked={value}
+                onChange={() => dispatch({type: 'toggle', index: i})}
+              />
+            </li>
+          );
+        })}
+      </ol>
     </main>
   );
 }
