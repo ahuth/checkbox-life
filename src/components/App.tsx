ff78c25f598ad6472e5c8b97b3cb0fea87bf18f1
Andrew Huth <git@huth.me>
2024-06-14 09:09:47 -0400

    Hello world

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,7 @@
-import {useReducer} from 'react';
-import Editor from './Editor';
-import Instructions from './Instructions';
-import Stack from './Stack';
-import {reducer, initialState} from '../reducer';
-
 export default function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
-
   return (
-    <main className="flex h-screen gap-2 bg-black p-4 text-white">
-      <div className="flex flex-col gap-2">
-        <Editor dispatch={dispatch} state={state} />
-        <Instructions />
-      </div>
-      <Stack state={state} />
+    <main>
+      <h1>Hello world</h1>
     </main>
   );
 }
