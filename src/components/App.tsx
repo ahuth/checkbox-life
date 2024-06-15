01ce0e1406bf3e3319cd8df2bb157b0c14bf75e7
Andrew Huth <git@huth.me>
2024-06-15 19:59:29 -0400

    Use useState instead of useReducer

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,31 +1,26 @@
-import {useReducer} from 'react';
+import {useState} from 'react';
 import Button from './Button';
 import {step} from '../step';
 
 const initialValues = new Array<number>(625).fill(0);
 
+function next(values: number[]) {
+  return step(values, Math.sqrt(initialValues.length));
+}
+
+function toggle(index: number, values: number[]) {
+  const nextState = values.slice();
+  nextState[index] = nextState[index] ? 0 : 1;
+  return nextState;
+}
+
 export default function App() {
-  const [values, dispatch] = useReducer(function (
-    prevState: number[],
-    action: {type: 'toggle'; index: number} | {type: 'step'},
-  ) {
-    if (action.type === 'toggle') {
-      const nextState = prevState.slice();
-      nextState[action.index] = nextState[action.index] ? 0 : 1;
-      return nextState;
-    }
-
-    if (action.type === 'step') {
-      return step(prevState, 25);
-    }
-
-    return prevState;
-  }, initialValues);
+  const [values, setValues] = useState(initialValues);
 
   return (
     <main>
       <div>
-        <Button onClick={() => dispatch({type: 'step'})}>Step</Button>
+        <Button onClick={() => setValues(next)}>Step</Button>
       </div>
       <ol className="grid grid-cols-[repeat(25,20px)]">
         {values.map((value, i) => {
@@ -34,7 +29,7 @@ export default function App() {
               <input
                 type="checkbox"
                 checked={value ? true : false}
-                onChange={() => dispatch({type: 'toggle', index: i})}
+                onChange={() => setValues(toggle.bind(null, i))}
               />
             </li>
           );
