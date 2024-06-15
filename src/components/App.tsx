94181a1bcd4a05e38d5a55ffa3662dab7265b0f3
Andrew Huth <git@huth.me>
2024-06-15 13:29:09 -0400

    Use 100 checkboxes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,25 +1,27 @@
 import {useReducer} from 'react';
 
-export default function App() {
-  const [values, dispatch] = useReducer(
-    function (prevState: boolean[], action: {type: 'toggle'; index: number}) {
-      if (action.type === 'toggle') {
-        const nextState = prevState.slice();
-        nextState[action.index] = !nextState[action.index];
-        return nextState;
-      }
+const initialValues = new Array(100).fill(false);
 
-      return prevState;
-    },
-    [false, false, false, false, false, false, false, false, false, false],
-  );
+export default function App() {
+  const [values, dispatch] = useReducer(function (
+    prevState: boolean[],
+    action: {type: 'toggle'; index: number},
+  ) {
+    if (action.type === 'toggle') {
+      const nextState = prevState.slice();
+      nextState[action.index] = !nextState[action.index];
+      return nextState;
+    }
+
+    return prevState;
+  }, initialValues);
 
   return (
     <main>
-      <ol>
+      <ol className="grid grid-cols-[repeat(10,20px)]">
         {values.map((value, i) => {
           return (
-            <li key={i}>
+            <li key={i} className="w-fit">
               <input
                 type="checkbox"
                 checked={value}
